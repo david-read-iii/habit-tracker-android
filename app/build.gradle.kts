a62ad0159730183cf4970b28eb2044a6886c7e6c
David Read <david-read-iii@users.noreply.github.com>
2025-08-21 21:17:56 -0400

    Add Compose compiler plugin to project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.jetbrains.kotlin.kapt)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
