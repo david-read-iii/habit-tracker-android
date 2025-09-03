f794cdb63f7f0a1a75403ff4b28a58f81825b4fa
David Read <david-read-iii@users.noreply.github.com>
2025-09-03 06:25:34 -0400

    Include Authentication header in all service calls excluding login and signup services.

--- app/src/main/java/com/davidread/habittracker/common/di/CommonModule.kt
+++ app/src/main/java/com/davidread/habittracker/common/di/CommonModule.kt
@@ -1,10 +1,13 @@
 package com.davidread.habittracker.common.di
 
 import com.davidread.habittracker.common.constant.BaseUrl
+import com.davidread.habittracker.common.network.AuthenticationInterceptor
+import com.davidread.habittracker.common.repository.AuthenticationTokenRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -13,13 +16,27 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class CommonModule {
 
-    // TODO: Add decrypted auth token in all service headers except login and signup.
     @Provides
     @Singleton
-    fun providesRetrofit(): Retrofit {
+    fun providesRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(BaseUrl.ANDROID_PHYSICAL_DEVICE_DEBUG)
+            .baseUrl(BaseUrl.ANDROID_EMULATOR_DEBUG)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(authenticationInterceptor: AuthenticationInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authenticationInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesAuthenticationInterceptor(authenticationTokenRepository: AuthenticationTokenRepository): AuthenticationInterceptor {
+        return AuthenticationInterceptor(authenticationTokenRepository)
     }
 }
