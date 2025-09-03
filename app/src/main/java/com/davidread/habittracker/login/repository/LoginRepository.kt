dff51de7fb42f154d607ea106826d75790f61b4d
David Read <david-read-iii@users.noreply.github.com>
2025-09-02 21:03:45 -0400

    Simplify logic in LoginUseCase. Use Result for service results.

--- app/src/main/java/com/davidread/habittracker/login/repository/LoginRepository.kt
+++ app/src/main/java/com/davidread/habittracker/login/repository/LoginRepository.kt
@@ -1,10 +1,16 @@
 package com.davidread.habittracker.login.repository
 
+import com.davidread.habittracker.common.model.Result
 import com.davidread.habittracker.login.model.LoginRequest
 import com.davidread.habittracker.login.service.LoginService
 import javax.inject.Inject
 
 class LoginRepository @Inject constructor(private val loginService: LoginService) {
 
-    suspend fun login(loginRequest: LoginRequest) = loginService.login(loginRequest)
+    suspend fun login(loginRequest: LoginRequest) = try {
+        val loginResponse = loginService.login(loginRequest)
+        Result.Success(loginResponse)
+    } catch (e: Exception) {
+        Result.Error(e)
+    }
 }
--- app/src/main/java/com/davidread/habittracker/login/usecase/LoginUseCase.kt
+++ app/src/main/java/com/davidread/habittracker/login/usecase/LoginUseCase.kt
@@ -3,6 +3,7 @@ package com.davidread.habittracker.login.usecase
 import android.app.Application
 import android.util.Log
 import com.davidread.habittracker.R
+import com.davidread.habittracker.common.model.Result
 import com.davidread.habittracker.login.model.DialogViewState
 import com.davidread.habittracker.login.model.LoginRequest
 import com.davidread.habittracker.login.model.LoginResult
@@ -15,7 +16,6 @@ import javax.inject.Inject
 
 private const val TAG = "LoginUseCase"
 
-// TODO: Simplify this logic somehow?
 class LoginUseCase @Inject constructor(
     private val application: Application,
     private val validateEmailUseCase: ValidateEmailUseCase,
@@ -28,11 +28,7 @@ class LoginUseCase @Inject constructor(
         val passwordValidationResult = validatePasswordUseCase(viewState.passwordTextFieldViewState)
 
         return if (emailValidationResult.status == Status.VALID && passwordValidationResult.status == Status.VALID) {
-            handleLoginServiceCall(
-                viewState = viewState,
-                emailValidationResult = emailValidationResult,
-                passwordValidationResult = passwordValidationResult
-            )
+            handleLoginServiceCall(viewState, emailValidationResult, passwordValidationResult)
         } else {
             LoginResult(
                 viewState = viewState.copy(
@@ -49,43 +45,44 @@ class LoginUseCase @Inject constructor(
         emailValidationResult: LoginTextFieldValidationResult,
         passwordValidationResult: LoginTextFieldValidationResult
     ): LoginResult {
-        return try {
-            val loginResponse = loginRepository.login(
-                loginRequest = LoginRequest(
-                    email = viewState.emailTextFieldViewState.value,
-                    password = viewState.passwordTextFieldViewState.value
+        val loginServiceResult = loginRepository.login(
+            loginRequest = LoginRequest(
+                email = viewState.emailTextFieldViewState.value,
+                password = viewState.passwordTextFieldViewState.value
+            )
+        )
+
+        return when (loginServiceResult) {
+            is Result.Success -> {
+                val token = loginServiceResult.data.token
+                // TODO: Store token somewhere somehow.
+                LoginResult(
+                    viewState = viewState.copy(
+                        emailTextFieldViewState = emailValidationResult.loginTextFieldViewState,
+                        passwordTextFieldViewState = passwordValidationResult.loginTextFieldViewState
+                    ),
+                    navigateToListScreen = true
                 )
-            )
-            // TODO: Store token somewhere somehow.
-            LoginResult(
-                viewState = viewState.copy(
-                    emailTextFieldViewState = emailValidationResult.loginTextFieldViewState,
-                    passwordTextFieldViewState = passwordValidationResult.loginTextFieldViewState
-                ),
-                navigateToListScreen = true
-            )
-        } catch (exception: Exception) {
-            Log.e(TAG, "Error logging in", exception)
-            if (exception is HttpException && exception.code() == 400) {
+            }
+
+            is Result.Error -> {
+                Log.e(TAG, "Error logging in", loginServiceResult.exception)
+                val message =
+                    if (loginServiceResult.exception is HttpException && loginServiceResult.exception.code() == 400) {
+                        application.getString(
+                            R.string.login_credentials_incorrect_error_message
+                        )
+                    } else {
+                        null
+                    }
                 LoginResult(
                     viewState = viewState.copy(
                         emailTextFieldViewState = emailValidationResult.loginTextFieldViewState,
                         passwordTextFieldViewState = passwordValidationResult.loginTextFieldViewState,
                         dialogViewState = DialogViewState(
                             showDialog = true,
-                            message = application.getString(
-                                R.string.login_credentials_incorrect_error_message
-                            )
+                            message = message
                         )
-                    ),
-                    navigateToListScreen = false
-                )
-            } else {
-                LoginResult(
-                    viewState = viewState.copy(
-                        emailTextFieldViewState = emailValidationResult.loginTextFieldViewState,
-                        passwordTextFieldViewState = passwordValidationResult.loginTextFieldViewState,
-                        dialogViewState = DialogViewState(showDialog = true, message = null)
                     ),
                     navigateToListScreen = false
                 )
