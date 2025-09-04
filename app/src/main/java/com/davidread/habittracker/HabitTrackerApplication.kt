f0406d4034a65f8a0c9dce961e72049d0ae2c6e1
David Read <david-read-iii@users.noreply.github.com>
2025-09-03 21:49:00 -0400

    Config AeadConfig on app startup in order to successfully save auth tokens to Tink.

--- app/src/main/java/com/davidread/habittracker/HabitTrackerApplication.kt
+++ app/src/main/java/com/davidread/habittracker/HabitTrackerApplication.kt
@@ -1,7 +1,14 @@
 package com.davidread.habittracker
 
 import android.app.Application
+import com.google.crypto.tink.aead.AeadConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HabitTrackerApplication : Application()
+class HabitTrackerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        AeadConfig.register()
+    }
+}
