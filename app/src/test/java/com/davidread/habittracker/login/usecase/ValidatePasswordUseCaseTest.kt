61f1902aa5b7beaeb9d3a83c264ae371908a8d11
David Read <david-read-iii@users.noreply.github.com>
2025-09-06 18:25:44 -0400

    Add unit tests for ValidatePasswordUseCase.

---
+++ app/src/test/java/com/davidread/habittracker/login/usecase/ValidatePasswordUseCaseTest.kt
@@ -0,0 +1,65 @@
+package com.davidread.habittracker.login.usecase
+
+import android.app.Application
+import com.davidread.habittracker.R
+import com.davidread.habittracker.login.model.LoginTextFieldValidationResult
+import com.davidread.habittracker.login.model.LoginTextFieldValidationResult.Status
+import com.davidread.habittracker.login.model.LoginTextFieldViewState
+import com.davidread.habittracker.testutil.MainDispatcherRule
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.After
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class ValidatePasswordUseCaseTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private val application = mockk<Application>()
+
+    private val validatePasswordUseCase = ValidatePasswordUseCase(application = application)
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun test_invoke_validPassword() {
+        val password = "longerthan8chars"
+        val expected = LoginTextFieldValidationResult(
+            status = Status.VALID,
+            loginTextFieldViewState = LoginTextFieldViewState(
+                value = password,
+                isError = false,
+                errorMessage = ""
+            )
+        )
+        val actual =
+            validatePasswordUseCase.invoke(viewState = LoginTextFieldViewState(value = password))
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_invoke_invalidPassword() {
+        val password = "pass"
+        val errorMessage = "Please enter a password with at least 8 characters"
+        every { application.getString(R.string.password_validation_error_message) } returns errorMessage
+
+        val expected = LoginTextFieldValidationResult(
+            status = Status.INVALID,
+            loginTextFieldViewState = LoginTextFieldViewState(
+                value = password,
+                isError = true,
+                errorMessage = errorMessage
+            )
+        )
+        val actual =
+            validatePasswordUseCase.invoke(viewState = LoginTextFieldViewState(value = password))
+        Assert.assertEquals(expected, actual)
+    }
+}
