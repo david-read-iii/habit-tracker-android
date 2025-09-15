1087d427800acdd0d9a22eb87969c97efc88ad3e
David Read <david-read-iii@users.noreply.github.com>
2025-09-14 21:21:26 -0400

    Specify return types explicitly per warning.

--- app/src/main/java/com/davidread/habittracker/common/util/Base64.kt
+++ app/src/main/java/com/davidread/habittracker/common/util/Base64.kt
@@ -5,7 +5,7 @@ import javax.inject.Inject
 
 class Base64 @Inject constructor() {
 
-    fun encodeToString(input: ByteArray, flags: Int) = Base64.encodeToString(input, flags)
+    fun encodeToString(input: ByteArray, flags: Int): String? = Base64.encodeToString(input, flags)
 
-    fun decode(str: String, flags: Int) = Base64.decode(str, flags)
+    fun decode(str: String, flags: Int): ByteArray? = Base64.decode(str, flags)
 }
