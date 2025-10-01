290242e621523c2ea52f85b3578b0e30e36312c3
David Read <david-read-iii@users.noreply.github.com>
2025-09-30 20:16:24 -0400

    Remove unused model.

--- app/src/main/java/com/davidread/habittracker/login/model/LoginTextFieldValidationResult.kt
+++
@@ -1,9 +0,0 @@
-package com.davidread.habittracker.login.model
-
-@Deprecated("Use common ValidationResult")
-data class LoginTextFieldValidationResult(
-    val status: Status,
-    val loginTextFieldViewState: LoginTextFieldViewState,
-) {
-    enum class Status { VALID, INVALID }
-}
