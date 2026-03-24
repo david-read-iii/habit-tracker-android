55d0d02633a7ebb9f179b071af4c6acfb2ef26f8
David Read <49120229+david-read-iii@users.noreply.github.com>
2026-03-24 12:49:19 -0400

    Add Room and Paging dependencies and set Java toolchain to version 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,12 +34,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
-        jvmToolchain(11)
+        jvmToolchain(17)
     }
 
     buildFeatures {
@@ -77,6 +77,12 @@ dependencies {
     implementation(libs.hilt.navigation.compose)
     implementation(libs.tink.android)
     implementation(libs.security.crypto)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    implementation(libs.room.paging)
+    kapt(libs.room.compiler)
+    implementation(libs.paging.runtime)
+    implementation(libs.paging.compose)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.leakcanary.android)
     debugImplementation(libs.compose.ui.test.manifest)
