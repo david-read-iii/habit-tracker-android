1ea7dd0dc0ae47772c5e1b12c1baed29808a27f6
David Read <49120229+david-read-iii@users.noreply.github.com>
2026-03-24 12:49:45 -0400

    Add debug and info methods to Logger.

--- app/src/main/java/com/davidread/habittracker/common/util/Logger.kt
+++ app/src/main/java/com/davidread/habittracker/common/util/Logger.kt
@@ -5,6 +5,14 @@ import javax.inject.Inject
 
 class Logger @Inject constructor() {
 
+    fun d(tag: String, message: String) {
+        Log.d(tag, message)
+    }
+
+    fun i(tag: String, message: String) {
+        Log.i(tag, message)
+    }
+
     fun e(tag: String, message: String, throwable: Throwable? = null) {
         Log.e(tag, message, throwable)
     }
