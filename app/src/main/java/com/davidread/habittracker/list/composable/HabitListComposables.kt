28e08a20f488f4a0c83676f4126b759f70738189
David Read <49120229+david-read-iii@users.noreply.github.com>
2026-04-04 23:55:04 -0400

    Add system bars padding to HabitList.

--- app/src/main/java/com/davidread/habittracker/list/composable/HabitListComposables.kt
+++ app/src/main/java/com/davidread/habittracker/list/composable/HabitListComposables.kt
@@ -13,11 +13,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -90,7 +93,9 @@ fun HabitListContent(
     habits: LazyPagingItems<HabitViewState>,
     onHabitClick: (String) -> Unit = {}
 ) {
-    Box(modifier = modifier.fillMaxSize()) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .padding(WindowInsets.systemBars.asPaddingValues())) {
         if (habits.loadState.refresh is LoadState.Loading) {
             LoadingListItem()
         } else {
