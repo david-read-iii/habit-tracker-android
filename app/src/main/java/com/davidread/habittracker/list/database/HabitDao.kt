bdf60d3974ebfcdd59683afaedb4464154463800
David Read <49120229+david-read-iii@users.noreply.github.com>
2026-04-06 13:00:13 -0400

    Remove ordering by createdAt from pagingSource query in HabitDao.

--- app/src/main/java/com/davidread/habittracker/list/database/HabitDao.kt
+++ app/src/main/java/com/davidread/habittracker/list/database/HabitDao.kt
@@ -13,7 +13,7 @@ interface HabitDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(habits: List<HabitEntity>)
 
-    @Query("SELECT * FROM ${DatabaseConstants.HABIT_TABLE_NAME} ORDER BY createdAt DESC")
+    @Query("SELECT * FROM ${DatabaseConstants.HABIT_TABLE_NAME}")
     fun pagingSource(): PagingSource<Int, HabitEntity>
 
     @Query("DELETE FROM ${DatabaseConstants.HABIT_TABLE_NAME}")
